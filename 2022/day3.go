5a8b448c53e879824cfbc45d45ec7e588baa3e4c
Simone Alessandro Casciaro <95073723+Alit54@users.noreply.github.com>
2022-12-03 16:48:24 +0100

    Added SplitStrings function

--- 2022/day3.go
+++ 2022/day3.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"bufio"
+
+	Alit "github.com/Alit54/AdventofCode/util"
+)
+
 func Day3(input string) (int, int) {
-	return 0, 0
+	return Part3A(input), Part3B(input)
+}
+
+func Part3A(input string) int {
+	// What is the sum of all the priorities?
+	sum := 0
+	file := Alit.File(input)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		row := Alit.SplitStrings()
+	}
+	return sum
 }
