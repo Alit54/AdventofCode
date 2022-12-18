096ae099b397d5eec92e50ebf636cb40e07beebd
Simone Alessandro Casciaro <95073723+Alit54@users.noreply.github.com>
2022-12-18 11:26:13 +0100

    Added Day12

--- 2022/day12.go
+++ 2022/day12.go
@@ -28,48 +28,31 @@ func Day12() (int, int) {
 }
 
 func Part12A() int {
-	matrix := make(map[Position]Node)
 	file := Alit.ReadMatrixByte(os.Args[1])
-	var pos Position
-	var node Node
-	// Create matrix
-	for i := 0; i < len(file); i++ {
-		for j := 0; j < len(file[i]); j++ {
-			pos.row = i
-			pos.column = j
-			node.high = file[i][j]
-			node.step = 0
-			matrix[pos] = node
-		}
-	}
-	// Search for position of start and goal
+	// Create Matrix
+	matrix := createMatrix(file)
+	// Search
 	start := find(matrix, 'S')
 	finish := find(matrix, 'E')
 	return UCSexl(start[0], matrix, finish)
 }
 
 func Part12B() int {
-	matrix := make(map[Position]Node)
 	file := Alit.ReadMatrixByte(os.Args[1])
-	var pos Position
-	var node Node
-	var min, tmp int
+	matrix := make(map[Position]Node)
+	min := Part12A()
+	var tmp int
 	// Create matrix
-	for i := 0; i < len(file); i++ {
-		for j := 0; j < len(file[i]); j++ {
-			pos.row = i
-			pos.column = j
-			node.high = file[i][j]
-			node.step = 0
-			matrix[pos] = node
-		}
-	}
+	matrix = createMatrix(file)
 	// Search
 	start := find(matrix, 'S')
 	start = append(start, find(matrix, 'a')...)
 	finish := find(matrix, 'E')
 	for _, v := range start {
+		matrix = createMatrix(file)
 		tmp = UCSexl(v, matrix, finish)
+		if tmp != Alit.Min() {
+		}
 		if tmp < min {
 			min = tmp
 		}
@@ -81,7 +64,7 @@ func Part12B() int {
 func find(matrice map[Position]Node, char byte) []Position {
 	slice := make([]Position, 0)
 	for k, v := range matrice {
-		if v.high == 'S' {
+		if v.high == char {
 			slice = append(slice, k)
 		}
 	}
@@ -103,7 +86,7 @@ func UCSexl(start Position, matrix map[Position]Node, goal []Position) int {
 		}
 		if !EXL[pos] {
 			// Found a goal
-			if len(Alit.ResearchElement(goal, pos)) != 0 {
+			if matrix[pos].high == 'E' {
 				return matrix[pos].step
 			}
 			EXL[pos] = true
@@ -134,4 +117,21 @@ func UCSexl(start Position, matrix map[Position]Node, goal []Position) int {
 		}
 	}
 	return Alit.Min()
+}
+
+// Creates the map required from a matrix of bytes.
+func createMatrix(input [][]byte) map[Position]Node {
+	matrix := make(map[Position]Node)
+	var pos Position
+	var node Node
+	for i := 0; i < len(input); i++ {
+		for j := 0; j < len(input[i]); j++ {
+			pos.row = i
+			pos.column = j
+			node.high = input[i][j]
+			node.step = 0
+			matrix[pos] = node
+		}
+	}
+	return matrix
 }
