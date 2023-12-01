33fa868cd175d4ef706ae249f0e36f26ddb74818
Simone Alessandro Casciaro <95073723+Alit54@users.noreply.github.com>
2023-12-01 21:36:37 +0100

    Added Day 1 Part B

--- 2023/day01.py
+++ 2023/day01.py
@@ -8,7 +8,6 @@ def part1():
     with open(file_path) as f:
         s = 0
         for line in f:
-            first = 0
             for letter in line:
                 if letter.isdigit():
                     first = letter
@@ -21,7 +20,46 @@ def part1():
         return s   
 
 def part2():
-    return 0
+    numbers = {
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
+    }
+    with open(file_path) as f:
+        s = 0
+        for line in f:
+            for i in range(len(line)):
+                if line[i].isdigit():
+                    first = line[i]
+                    break
+                else:     
+                    for j in range(i+2, len(line)):
+                        if line[i:j] in numbers:
+                            first = numbers[line[i:j]]
+                            break
+                    else:
+                        continue
+                    break
+            for i in range(len(line))[::-1]:
+                if line[i].isdigit():
+                    last = line[i]
+                    break
+                else:     
+                    for j in range(0, i)[::-1]:
+                        if line[j:i+1] in numbers:
+                            last = numbers[line[j:i+1]]
+                            break
+                    else:
+                        continue
+                    break
+            s += int(first + last)
+        return s  
 
 
 print(f'Part 1: {part1()}')
