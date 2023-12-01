e7e74b44f3b76db6cd7c2c05d6f71b14d7bed621
Simone Alessandro Casciaro <95073723+Alit54@users.noreply.github.com>
2023-12-01 07:23:27 +0100

    Day 1 Part 1

--- 2023/day01.py
+++ 2023/day01.py
@@ -1,2 +1,26 @@
 ### First AOC Day in Python!
 # Read the question here: https://adventofcode.com/2023/day/1
+
+file_path = 'input/day01.txt'
+file_prova = 'input/prova.txt'
+
+def part1():
+    with open(file_path) as f:
+        s = 0
+        for line in f:
+            first = 0
+            last = 0
+            for letter in line:
+                if letter.isdigit():
+                    if first == 0:
+                        first = letter
+                    last = letter
+            s += int(first + last)
+        return s   
+
+def part2():
+    return 0
+
+
+print(f'Part 1: {part1()}')
+print(f'Part 2: {part2()}')
